34e9b7c88a38ea964271fa5812dce490099577d2
YMB01 <25132dev@gmail.com>
2024-09-17 17:37:18 +0300

    primitive type

---
+++ Typescript Types/Lessons/index.ts
@@ -0,0 +1,20 @@
+//Type Assignment
+//Explicit
+let firstName : string = 'Jo';
+//Implicit
+let lastName = 'Doe';
+//number
+let age : number = 20;
+//boolean
+let human: boolean = true;
+//void
+function  sayHello(): void {
+    console.log("Hello!"); 
+}
+//Undefined
+let value:  undefined = undefined;
+//null
+let zero: null = null;
+
+
+
